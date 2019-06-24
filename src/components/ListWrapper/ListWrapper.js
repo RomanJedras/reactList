6dc3808ac1e30bd5426c4e889b5ae2ef05e4db65
Roman Jędras <romanjedras@o2.pl>
2019-06-24 15:34:54 +0200

    Spread on item operation

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -6,12 +6,8 @@ import {twitterAccounts} from "../../data/twitterAccounts";
 const ListWrapper = (props) => (
 		<ul className={'listWrapper_list'}>
 			
-			{twitterAccounts.map(({name,description,image,twitterLink}) => (
-				<ListItem  name={name}
-				           description={description}
-				           image={image}
-				           link = {twitterLink}
-				/>
+			{twitterAccounts.map(item => (
+				<ListItem {...item} />
 			))}
 		</ul>
 )

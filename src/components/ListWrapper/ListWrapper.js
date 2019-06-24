47c3d678975f8652af442019353572b8a2085edf
Roman Jędras <romanjedras@o2.pl>
2019-06-24 15:33:01 +0200

    Destruction operator

--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -6,7 +6,7 @@ import {twitterAccounts} from "../../data/twitterAccounts";
 const ListWrapper = (props) => (
 		<ul className={'listWrapper_list'}>
 			
-			{twitterAccounts.map(({name,description,image,twitterLink}) => (
+			{twitterAccounts.map(( => (
 				<ListItem  name={name}
 				           description={description}
 				           image={image}

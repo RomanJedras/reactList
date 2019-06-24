13e8e98486f3727e00dba2482ec8e233ee41d7da
Roman Jędras <romanjedras@o2.pl>
2019-06-24 15:42:15 +0200

    Add destruction operator to React on ItemList

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './ListItem.css'
 
 
-const ListItem = (props) => (
+const ListItem = ({
+	name, image, description, twitterLink}) => (
 	 <li className={'listItem__wrapper'}>
-		 <img alt={props.name || ''} src={props.image} className={'listItem__image'}/>
+		 <img alt={name || ''} src={image} className={'listItem__image'}/>
 		 <div>
-			 <h2 className={'listItem__name'}>{props.name}</h2>
-			 <p className={'listItem__description'}>{props.description}</p>
-			 <a href={props.link} className={'listItem__button'}>visit twitter page</a>
+			 <h2 className={'listItem__name'}>{name}</h2>
+			 <p className={'listItem__description'}>{description}</p>
+			 <a href={twitterLink} className={'listItem__button'}>visit twitter page</a>
 		 </div>
 	 </li>
 	 
